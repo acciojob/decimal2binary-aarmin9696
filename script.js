88b398ffa3b3a290316e9c7bb109cd93aa4e50de
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 13:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/decimalToBinary', (req, res) => {
+  const decimal = req.body.n;
+  const binary = decimalToBinary(decimal);
+  res.json({ result: binary });
+});
+
 function decimalToBinary(decimal) {
-  let binary = "";
+  // Conversion logic here
+  // ...
+	let binary = "";
   while (decimal > 0) {
     binary = (decimal % 2) + binary;
     decimal = Math.floor(decimal / 2);
   }
   return binary;
 }
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+
