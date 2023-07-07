518fa07d64c6c2b7ab68beeedcfdcc433473649c
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:16:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-
-
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.post('/decimalToBinary', (req, res) => {
-  const decimal = req.body.n;
-  const binary = decimalToBinary(decimal);
-  res.json({ result: binary });
-});
-
-function decimalToBinary(decimal) {
-  // Conversion logic here
-  // ...
-	let binary = "";
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-
-
-
+window.decimalToBinary = decimalToBinary;
